c8f0f65c5cf8187ee70069bbac81d168910df656
Firdavs Khaydarov <recouse@recouse.email>
2023-04-15 17:44:30 +0300

    Moved `Configuration` to the separate file

---
+++ Sources/OpenAI/Configuration.swift
@@ -0,0 +1,33 @@
+//
+//  Configuration.swift
+//  OpenAI
+//
+//  Created by Firdavs Khaydarov on 15/04/2023.
+//
+
+extension OpenAI {
+    struct Configuration {
+        /// Open AI API key
+        let apiKey: String
+        
+        /// Organization ID
+        ///
+        /// For users who belong to multiple organizations, you can specify which organization
+        /// is used for an API request. Usage from these API requests will count against
+        /// the specified organization's subscription quota.
+        let organization: String?
+        
+        var headers: HTTPHeaders {
+            var headers: HTTPHeaders = [
+                "Authorization": "Bearer \(apiKey)",
+                "Content-Type": "application/json"
+            ]
+            
+            if let organization {
+                headers["OpenAI-Organization"] = organization
+            }
+            
+            return headers
+        }
+    }
+}
--- Sources/OpenAI/OpenAI.swift
+++ Sources/OpenAI/OpenAI.swift
@@ -10,31 +10,6 @@ import Foundation
 import os.log
 
 public struct OpenAI {
-    struct Configuration {
-        /// Open AI API key
-        let apiKey: String
-        
-        /// Organization ID
-        ///
-        /// For users who belong to multiple organizations, you can specify which organization
-        /// is used for an API request. Usage from these API requests will count against
-        /// the specified organization's subscription quota.
-        let organization: String?
-        
-        var headers: HTTPHeaders {
-            var headers: HTTPHeaders = [
-                "Authorization": "Bearer \(apiKey)",
-                "Content-Type": "application/json"
-            ]
-            
-            if let organization {
-                headers["OpenAI-Organization"] = organization
-            }
-            
-            return headers
-        }
-    }
-    
     let configuration: Configuration
     
     let requestHandler: RequestHandler
