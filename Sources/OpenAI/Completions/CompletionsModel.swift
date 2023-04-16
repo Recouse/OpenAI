557e9e2dab95cab09c00d0716c5d45927c467377
Firdavs Khaydarov <recouse@recouse.email>
2023-04-17 00:45:23 +0300

    Updated models case names

--- Sources/OpenAI/Completions/CompletionsModel.swift
+++ Sources/OpenAI/Completions/CompletionsModel.swift
@@ -14,28 +14,28 @@ public extension Completions {
         /// Also supports inserting completions within text.
         /// * Max tokens: 4,097
         /// * Training data: Up to Jun 2021
-        case text_davinci_003 = "text-davinci-003"
+        case textDavinci003 = "text-davinci-003"
         
         /// Similar capabilities to text-davinci-003 but trained with supervised fine-tuning instead of reinforcement learning
         /// * Max tokens: 4,097
         /// * Training data: Up to Jun 2021
-        case text_davinci_002 = "text-davinci-002"
+        case textDavinci002 = "text-davinci-002"
         
         // MARK: - GPT-3
         
         /// Very capable, faster and lower cost than Davinci.
         /// * Max tokens: 2,049
         /// * Training data: Up to Oct 2019
-        case text_curie_001 = "text-curie-001"
+        case textCurie001 = "text-curie-001"
         
         /// Capable of straightforward tasks, very fast, and lower cost.
         /// * Max tokens: 2,049
         /// * Training data: Up to Oct 2019
-        case text_babbage_001 = "text-babbage-001"
+        case textBabbage001 = "text-babbage-001"
         
         /// Capable of very simple tasks, usually the fastest model in the GPT-3 series, and lowest cost.
         /// * Max tokens: 2,049
         /// * Training data: Up to Oct 2019
-        case text_ada_001 = "text-ada-001"
+        case textAda001 = "text-ada-001"
     }
 }
