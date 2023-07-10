0870b796d660ae7907c513bd873aaff7a3f674cf
Firdavs Khaydarov <recouse@recouse.email>
2023-07-10 19:44:21 +0300

    Added new GPT models

--- Sources/OpenAI/Chat/ChatCompletionsModels.swift
+++ Sources/OpenAI/Chat/ChatCompletionsModels.swift
@@ -21,6 +21,12 @@ public extension ChatCompletions {
         /// * Training data: Up to Sep 2021
         case gpt4_0314 = "gpt-4-0314"
         
+        /// Snapshot of gpt-4 from June 13th 2023 with function calling data. Unlike gpt-4,
+        /// this model will not receive updates, and will be deprecated 3 months after a new version is released.
+        /// * Max tokens: 8,192
+        /// * Training data: Up to Sep 2021
+        case gpt4_0613 = "gpt-4-0613"
+        
         /// Same capabilities as the base gpt-4 mode but with 4x the context length. Will be updated with our latest model iteration.
         /// * Max tokens: 32,768
         /// * Training data: Up to Sep 2021
@@ -32,6 +38,12 @@ public extension ChatCompletions {
         /// * Training data: Up to Sep 2021
         case gpt4_32k_0314 = "gpt-4-32k-0314"
         
+        /// Snapshot of gpt-4-32 from June 13th 2023. Unlike gpt-4-32k, this model will not receive updates,
+        /// and will be deprecated 3 months after a new version is released.
+        /// * Max tokens: 32,768
+        /// * Training data: Up to Sep 2021
+        case gpt4_32k_0613 = "gpt-4-32k-0613"
+        
         // MARK: - GTP-3.5
         
         /// Most capable GPT-3.5 model and optimized for chat at 1/10th the cost of text-davinci-003.
@@ -40,10 +52,27 @@ public extension ChatCompletions {
         /// * Training data: Up to Sep 2021
         case gpt3_5_turbo = "gpt-3.5-turbo"
         
+        /// Same capabilities as the standard `gpt-3.5-turbo` model but with 4 times the context.
+        /// * Max tokens: 16,384
+        /// * Training data: Up to Sep 2021
+        case gpt3_5_turbo_16k = "gpt-3.5-turbo-16k"
+        
         /// Snapshot of gpt-3.5-turbo from March 1st 2023. Unlike gpt-3.5-turbo, this model will not receive updates,
         /// and will only be supported for a three month period ending on June 1st 2023.
         /// * Max tokens: 4,096
         /// * Training data: Up to Sep 2021
         case gpt3_5_turbo_0301 = "gpt-3.5-turbo-0301"
+        
+        /// Snapshot of gpt-3.5-turbo from June 13th 2023 with function calling data. Unlike gpt-3.5-turbo,
+        /// this model will not receive updates, and will be deprecated 3 months after a new version is released.
+        /// * Max tokens: 4,096
+        /// * Training data: Up to Sep 2021
+        case gpt3_5_turbo_0613 = "gpt-3.5-turbo-0613"
+        
+        /// Snapshot of gpt-3.5-turbo-16k from June 13th 2023. Unlike gpt-3.5-turbo-16k,
+        /// this model will not receive updates, and will be deprecated 3 months after a new version is released.
+        /// * Max tokens: 16,384
+        /// * Training data: Up to Sep 2021
+        case gpt3_5_turbo_16k_0613 = "gpt-3.5-turbo-16k-0613"
     }
 }
