0464ca2360b448943bce26d3c477a7f1c44e8b2a
Firdavs Khaydarov <recouse@recouse.email>
2024-01-27 00:45:17 +0200

    Updated EventSource to version 0.0.6

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/Recouse/EventSource.git", from: "0.0.5")
+        .package(url: "https://github.com/Recouse/EventSource.git", from: "0.0.6")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
